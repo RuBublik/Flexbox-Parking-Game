ca0f5b08ada9b9e22eabe2e7ffdcd345a9f6080a
OFEKD102 <ofekdrikes@gmail.com>
2026-09-09 17:49:46 +0300

    implement collision logic

--- js/collision.js
+++ js/collision.js
@@ -1,5 +1,20 @@
 export class CollisionDetector {
     static isAllAligned(cars, spots) {
-        //to do
+        if (cars.length === 0 || cars.length !== spots.length) return false;
+
+        const threshold = 15;
+
+        for (let i = 0; i < cars.length; i++) {
+            const carRect = cars[i].getBoundingClientRect();
+            const spotRect = spots[i].getBoundingClientRect();
+
+            const xDiff = Math.abs(carRect.left - spotRect.left);
+            const yDiff = Math.abs(carRect.top - spotRect.top);
+
+            if (xDiff > threshold || yDiff > threshold) {
+                return false;
+            }
+        }
+        return true;
     }
 }
